8cd9ed4626cb69e0f018cab1b2da976f0264c7c7
Ing. Ebu Celik <ebucelik1@hotmail.com>
2023-03-25 00:43:32 +0100

    Initial Commit

---
+++ GetFact/GetFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetFactApp.swift
+//  GetFact
+//
+//  Created by Ing. Ebu Celik, BSc on 25.03.23.
+//
+
+import SwiftUI
+
+@main
+struct GetFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
